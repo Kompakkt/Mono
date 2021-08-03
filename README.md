ce9b5bd13e36e5d136d0a8d0d0e9536e50c40be9
Kai Niebes <kai.niebes@outlook.com>
2021-08-03 15:02:45 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Kompakkt Mono
+
+## A collection of scripts to get a Kompakkt development environment up and running
+
---
+++ setup.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+cd Viewer
+npm install --no-optional
+cd ..
+
+cd Repo
+npm install --no-optional
+cd ..
+
+cd Server
+npm install --no-optional
+cd ..
